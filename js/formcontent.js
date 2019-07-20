dff59cf5972d82cb87a993259ec99b4eaddc513f
BlizzardsFlame <ned.r@hotmail.fr>
2019-07-20 02:54:23 +0200

    just modified the form

--- js/formcontent.js
+++ js/formcontent.js
@@ -1,4 +1,4 @@
-//Profession
+// Profession list
 var profession_arr = new Array(
     "Artisant sédentaire","Artisant non sédentaire","Commerçant",
     "Commerçant non sédentaire","Ecclésiastique","Etudiant",
@@ -7,14 +7,32 @@ var profession_arr = new Array(
     "Retraité","Salarié","Salarié non sédentaire","Sans profession",
     "Taxi","TPM","Véhicule société","VRP");
 
-    function populateProfession(professionElementId){
+    // Profession list population
         // given the id of the <select> tag as function argument, it inserts <option> tags
-        var professionElement = document.getElementById(professionElementId);
+        var professionElement = document.getElementById("Profession");
         professionElement.length=0;
-        professionElement.options[0] = new Option('Selectioner une profession','-1');
+        professionElement.options[0] = new Option('Selectioner une profession','');
         professionElement.selectedIndex = 0;
         for (var i=0; i<profession_arr.length; i++) {
             professionElement.options[professionElement.length] =
             new Option(profession_arr[i],profession_arr[i]);
         }
+
+// Form validation
+var form = document.querySelector('.needs-validation');
+
+form.addEventListener('submit', function(event){
+    if(form.checkValidity() === false){
+        event.preventDefault();
+        event.stopPropagation();
     }
+    form.classList.add('was-validated');
+})
+
+// range control spinner value
+var slider = document.getElementById("coefMalus");
+        var output = document.getElementById("valeurMalus");
+        output.innerHTML = slider.value;
+        slider.oninput = function() {
+            output.innerHTML = this.value;
+        }
